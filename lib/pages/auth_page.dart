c93762aa4626bd73b9df416a5ce7b5e47e247505
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2023-12-06 18:38:09 -0300

    readequação da estrutura do projeto

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,5 @@
 import 'package:chat/components/auth_form.dart';
-import 'package:chat/models/auth_form_data.dart';
+import 'package:chat/core/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -11,10 +11,22 @@ class AuthPage extends StatefulWidget {
 
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
 
-    print(formData.email);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
+      if (formData.isLogin) {
+        //
+      } else {
+        //
+      }
+    } catch (error) {
+      //
+    } finally {
+      setState(() => _isLoading = false);
+    }
+
+    //print(formData.email);
     //setState(() => _isLoading = false);
   }
 
